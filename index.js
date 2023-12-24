8af6ad4343e8a200548c83b06c563b54067526d8
Vaibhav-Yerkar <vaibhavyerkar2017@gmail.com>
2023-12-24 22:19:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const state = {
+    taskList : [],
+};
+
+const taskContents = document.querySelector("task__contents");
+const taskModal = document.querySelector("task__modal__body");
+
+console.log(taskContents);
